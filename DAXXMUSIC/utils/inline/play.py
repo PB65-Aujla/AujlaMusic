5b448fdc3ac49e47e7b4014ec70de876a77f9e09
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2023-12-31 12:59:30 +0400

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -68,14 +68,14 @@ def stream_markup_timer(_, chat_id, played, dur):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="🖱️ 𝙾𝚆𝙽𝙴𝚁",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="𝙶𝚁𝙾𝚄𝙿 🖱️",
 
                 url=f"{SUPPORT_CHAT}",
 
@@ -97,14 +97,14 @@ def stream_markup(_, chat_id):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="🖱️ 𝙾𝚆𝙽𝙴𝚁",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="𝙶𝚁𝙾𝚄𝙿 🖱️",
 
                 url=f"{SUPPORT_CHAT}",
 
